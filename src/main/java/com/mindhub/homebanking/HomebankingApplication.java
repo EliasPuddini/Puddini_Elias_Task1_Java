5be110b9aaa1819f4905e713d6cab47dcdbe72b0
EliasPuddini1 <epuddini@frba.utn.edu.ar>
2023-08-04 15:52:40 -0300

    Primer import

---
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -0,0 +1,29 @@
+package com.mindhub.homebanking;
+
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner init(ClientRepository clientRepository){
+		return args -> {
+			
+			Client client = new Client("Elias", "Puddini", "epuddini@gmail.com");
+			Client client2 = new Client("", "","fpagadorio@gmail.com");
+			Client client3 = new Client("Diego","Maradona","riverElMasGrande@gmail.com");
+			clientRepository.save(client);
+			clientRepository.save(client2);
+			clientRepository.save(client3);
+		};
+	}
+}
