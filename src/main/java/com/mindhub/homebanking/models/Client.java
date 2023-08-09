7b5c51fd3e0422e13dcd4d755b8df81cfdedef50
EliasPuddini <97805053+EliasPuddini@users.noreply.github.com>
2023-08-08 23:50:50 -0300

    Actualizacion

--- src/main/java/com/mindhub/homebanking/models/Client.java
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -1,11 +1,11 @@
 package com.mindhub.homebanking.models;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.loader.collection.OneToManyJoinWalker;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Client {
@@ -16,6 +16,9 @@ public class Client {
     private String firstName;
     private String lastName;
     private String email;
+
+    @OneToMany(fetch = FetchType.EAGER,mappedBy = "client")
+    private Set<Account> accounts = new HashSet<>();
 
 
     public Client() { }
@@ -49,6 +52,14 @@ public class Client {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+    public void addAccount(Account account){
+        account.setClient(this);
+        this.accounts.add(account);
     }
 
     @Override
